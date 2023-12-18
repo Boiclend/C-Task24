1e676233ffaef58776d734add6180e798c3ab1f0
unknown <igoshin1922@gmail.com>
2023-12-19 03:14:51 +0500

    added task 24

---
+++ Task24/Program.cs
@@ -0,0 +1,30 @@
+﻿// дополнение к прошлой задаче Task23 , только здесь сортировка слов.
+
+string path = @"C:\Users\РомаиЛиля\Desktop\C#Task24\Task24\words.txt";
+string text = File.ReadAllText(path); // чтение файла
+
+string temp = text.ToLower(); // привожу к нижнему регистру, потому что если будут символы верхнего регистра, то они не будут удалены как дубликаты.
+char[] txt = temp.ToArray();
+string[] s = new string[text.Length];
+int count = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    if(txt[i] == ' ')
+    {
+        count++;
+    }
+    if(txt[i] != ' ') 
+    {
+        s[count] = s[count] + txt[i].ToString();
+    }
+    
+}
+s = s.Distinct().ToArray();
+Array.Sort(s);
+
+for (int i = 0; i < s.Length; i++)
+{
+    Console.WriteLine(s[i]);   
+}
+
+
